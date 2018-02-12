68a0c634495a66868dac13176a0c2110aeb8b284
Andriy Khomych <andriy.khomych@gmail.com>
2018-02-12 18:29:55 +0200

    Date time day:
    Added base functional tests and planned base test methods.

---
+++ tests/src/Functional/DateTimeDayFieldTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\Tests\date_time_day\Functional;
+
+use Drupal\Tests\datetime\Functional\DateTestBase;
+
+/**
+ * Tests date_time_day field functionality.
+ *
+ * @group date_time_day
+ */
+class DateTimeDayFieldTest extends DateTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['date_time_day'];
+
+  /**
+   * The default display settings to use for the formatters.
+   *
+   * @var array
+   */
+  protected $defaultSettings = [
+    'timezone_override' => '',
+    'date_separator' => '-',
+    'time_separator' => ',',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getTestFieldType() {
+    return 'date_time_day';
+  }
+
+  /**
+   * Test the default field type.
+   */
+  public function testDateTimeDayTypeDefaultField() {
+
+  }
+
+  /**
+   * Test with seconds field type.
+   */
+  public function testDateTimeDayTypeSecondsField() {
+
+  }
+
+  /**
+   * Test the default field widget.
+   */
+  public function testDateTimeDayDefaultWidgetField() {
+
+  }
+
+  /**
+   * Test with seconds field widget.
+   */
+  public function testDateTimeDaySecondsWidgetField() {
+
+  }
+
+  /**
+   * Test default value functionality.
+   */
+  public function testDefaultValue() {
+
+  }
+
+  /**
+   * Test that invalid values are caught and marked as invalid.
+   */
+  public function testInvalidField() {
+
+  }
+
+  /**
+   * Tests that 'Date' field storage setting form is disabled if field has data.
+   */
+  public function testDateStorageSettings() {
+
+  }
+
+}
