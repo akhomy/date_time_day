59d9477bb4f622d56a710115bb4f5fb26cf3f485
Andrey Rachinskiy <55188-Rachinskiy@users.noreply.drupalcode.org>
2023-04-23 21:27:44 +0300

    Issue #3354186 by Rachinskiy: Deprecated function: DateTime::createFromFormat(): Passing null to parameter #2 ($datetime) of type string is deprecated in Drupal\Component\Datetime\DateTimePlus::createFromFormat() (line 251 of core/lib/Drupal/Component/Datetime/DateTimePlus.php)

--- src/DateDayComputed.php
+++ src/DateDayComputed.php
@@ -45,7 +45,7 @@ class DateDayComputed extends TypedData {
 
     /** @var \Drupal\Core\Field\FieldItemInterface $item */
     $item = $this->getParent();
-    $value = $item->{($this->definition->getSetting('date source'))};
+    $value = $item->{($this->definition->getSetting('date source'))} ?? '';
 
     $storage_format = DateTimeItemInterface::DATE_STORAGE_FORMAT;
     try {
--- src/DateTimeDayComputed.php
+++ src/DateTimeDayComputed.php
@@ -46,7 +46,7 @@ class DateTimeDayComputed extends TypedData {
 
     /** @var \Drupal\Core\Field\FieldItemInterface $item */
     $item = $this->getParent();
-    $value = $item->{($this->definition->getSetting('date source'))};
+    $value = $item->{($this->definition->getSetting('date source'))} ?? '';
     $datetime_type = $item->getFieldDefinition()->getSetting('time_type');
     $storage_format = $datetime_type === DateTimeDayItem::DATEDAY_TIME_DEFAULT_TYPE_FORMAT ? DateTimeDayItem::DATE_TIME_DAY_H_I_FORMAT_STORAGE_FORMAT : DateTimeDayItem::DATE_TIME_DAY_H_I_S_FORMAT_STORAGE_FORMAT;
     // Fix time with seconds in incorrect widget.
